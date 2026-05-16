be3f0eeb6d56dca977be9a2a525d14bb8437dbda
ishofstede <101320849+ishofstede@users.noreply.github.com>
2026-05-16 15:08:39 +0200

    logic setup for controller and analysis

--- src/main/java/nl/bioinf/ishofstede/controller/SequenceController.java
+++ src/main/java/nl/bioinf/ishofstede/controller/SequenceController.java
@@ -1,4 +1,19 @@
 package nl.bioinf.ishofstede.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SequenceController {
+
+    //homepage mapping init
+
+    //analysispage mapping init
+
+    // recieve dna seq input from user req
+    //returns true if valid, otherwise false
+    //store seq so it can be shown in results page
+    //store valid result for display
+    //return results
+
+
 }
--- src/main/java/nl/bioinf/ishofstede/service/AnalysisService.java
+++ src/main/java/nl/bioinf/ishofstede/service/AnalysisService.java
@@ -1,4 +1,14 @@
 package nl.bioinf.ishofstede.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnalysisService {
+
+    public boolean isValidDNA(String sequence){
+        //if seq = null return false
+    }
+    //check for whitespace and ignore it
+    //to upper case
+    //return seq if matching ACGT
 }
