c92427f387d6a858dbc3598b6abedcd5dc9d4b2e
ishofstede <101320849+ishofstede@users.noreply.github.com>
2026-06-01 16:40:27 +0200

    Added enzyme list to AnalysisService

--- src/main/java/nl/bioinf/ishofstede/service/AnalysisService.java
+++ src/main/java/nl/bioinf/ishofstede/service/AnalysisService.java
@@ -3,6 +3,9 @@ package nl.bioinf.ishofstede.service;
 import org.springframework.stereotype.Service;
 import nl.bioinf.ishofstede.model.RestrictionEnzyme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AnalysisService {
     /**
@@ -45,4 +48,28 @@ public class AnalysisService {
 
         return sequence.matches("[ACGT]+");
     }
+
+    public List<RestrictionEnzyme> getEnzymes() {
+
+        List<RestrictionEnzyme> enzymes = new ArrayList<>();
+
+        enzymes.add(new RestrictionEnzyme("AluI", "AGCT"));
+        enzymes.add(new RestrictionEnzyme("HaeIII", "GGCC"));
+        enzymes.add(new RestrictionEnzyme("MspI", "CCGG"));
+        enzymes.add(new RestrictionEnzyme("XhoI", "CTCGAG"));
+        enzymes.add(new RestrictionEnzyme("SalI", "GTCGAC"));
+        enzymes.add(new RestrictionEnzyme("PstI", "CTGCAG"));
+        enzymes.add(new RestrictionEnzyme("EcoRI", "GAATTC"));
+        enzymes.add(new RestrictionEnzyme("BamHI", "GGATCC"));
+        enzymes.add(new RestrictionEnzyme("HindIII", "AAGCTT"));
+        enzymes.add(new RestrictionEnzyme("NotI", "GCGGCCGC"));
+        enzymes.add(new RestrictionEnzyme("KpnI", "GGTACC"));
+        enzymes.add(new RestrictionEnzyme("SmaI", "CCCGGG"));
+        enzymes.add(new RestrictionEnzyme("EcoRV", "GATATC"));
+        enzymes.add(new RestrictionEnzyme("DraI", "TTTAAA"));
+        enzymes.add(new RestrictionEnzyme("NcoI", "CCATGG"));
+
+        return enzymes;
+    }
+
 }
